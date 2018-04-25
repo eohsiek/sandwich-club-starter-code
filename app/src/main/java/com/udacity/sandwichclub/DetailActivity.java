f4cb7f0c3b9eff82fe84d2e595c6c96290d838ba
eohsiek <eohsiek@gmail.com>
2018-04-24 21:25:20 -0600

    mostly working - needs layout cleanup

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
@@ -21,6 +22,7 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
 
         ImageView ingredientsIv = findViewById(R.id.image_iv);
+
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -43,7 +45,7 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -56,7 +58,17 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
+        TextView mDescriptionView = findViewById(R.id.description_tv);
+        mDescriptionView.setText(sandwich.getDescription());
 
+        TextView mPlaceofOriginView = findViewById(R.id.origin_tv);
+        mPlaceofOriginView.setText(sandwich.getPlaceOfOrigin());
+
+        TextView mIngredientsView = findViewById(R.id.ingredients_tv);
+        mIngredientsView.setText(sandwich.getIngredients().toString().replace("[", "").replace("]", "").replace(",", ", "));
+
+        TextView mAlsoKnownAsView = findViewById(R.id.also_known_tv);
+        mAlsoKnownAsView.setText(sandwich.getAlsoKnownAs().toString().replace("[", "").replace("]", "").replace(",", ", "));
     }
 }
