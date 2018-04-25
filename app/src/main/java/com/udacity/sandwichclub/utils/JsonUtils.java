b5594ffdde99d0fc94f4af20932aa1f1d934c15c
eohsiek <eohsiek@gmail.com>
2018-04-24 20:14:19 -0600

    working json parse

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,39 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+        try {
+            JSONObject sandwichJson = new JSONObject(json);
+            JSONObject name = sandwichJson.getJSONObject("name");
+            String mainName = name.getString("mainName");
+            String placeOfOrigin = sandwichJson.getString("placeOfOrigin");
+            String description = sandwichJson.getString("description");
+            String image = sandwichJson.getString("image");
+            JSONArray namearray = name.getJSONArray("alsoKnownAs");
+            List<String> alsoKnownAs = new ArrayList<>();
+            for (int i = 0; i < namearray.length(); i++) {
+                alsoKnownAs.add(namearray.getString(i));
+            }
+            JSONArray ingredientarray = sandwichJson.getJSONArray("ingredients");
+            List<String> ingredients = new ArrayList<>();
+            for (int i = 0; i < ingredientarray.length(); i++) {
+                ingredients.add(ingredientarray.getString(i));
+            }
+            return new Sandwich(mainName, alsoKnownAs, placeOfOrigin, description, image, ingredients);
+        }  catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+
+
     }
 }
